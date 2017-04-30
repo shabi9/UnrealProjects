63cbc58a098470d6c3ec5cb279babeddffd41194
shahbaz Khan <shahbaz-khan09@hotmail.com>
2017-04-30 23:24:18 +0300

    Initial Commit

---
+++ EscapeTheRoom/Source/EscapeTheRoom/EscapeTheRoomGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "EscapeTheRoom.h"
+#include "EscapeTheRoomGameModeBase.h"
+
+
+
+
---
+++ EscapeTheRoom/Source/EscapeTheRoom/EscapeTheRoomGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "EscapeTheRoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPETHEROOM_API AEscapeTheRoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
